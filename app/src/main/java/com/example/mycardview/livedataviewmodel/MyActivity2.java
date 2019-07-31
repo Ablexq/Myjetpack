9c258b0a2edb95186e08d776f67dad26d5636bae
ablexq <776356314@qq.com>
2019-07-31 15:55:26 +0800

    scardview的源码

--- app/src/main/java/com/example/mycardview/livedataviewmodel/MyActivity2.java
+++ app/src/main/java/com/example/mycardview/livedataviewmodel/MyActivity2.java
@@ -2,36 +2,87 @@ package com.example.mycardview.livedataviewmodel;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.Observable;
+import androidx.databinding.ObservableField;
 import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-public class MyActivity2 extends FragmentActivity {
+import com.example.mycardview.R;
+
+public class MyActivity2 extends FragmentActivity implements View.OnClickListener {
+
+    private MyViewModel viewModel;
+    private TextView textView1;
+    private ObservableUser observableUser;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
 
-        final MyViewModel viewModel = ViewModelProviders.of(MyActivity2.this).get(MyViewModel.class);
+        initViews();
+
+        viewModel = ViewModelProviders.of(MyActivity2.this).get(MyViewModel.class);
         viewModel.getName().observe(this, new Observer<String>() {
             @Override
             public void onChanged(String s) {
                 System.out.println("s===============" + s);//实时监听
+                textView1.setText(s);
             }
         });
 
-        new Button(this).setOnClickListener(new View.OnClickListener() {
+        observableUser = new ObservableUser();
+        observableUser.name.addOnPropertyChangedCallback(new Observable.OnPropertyChangedCallback() {
             @Override
-            public void onClick(View v) {
-                //主线程
-                viewModel.getName().setValue("666");
-                //子线程
-                viewModel.getName().postValue("999");
+            public void onPropertyChanged(Observable sender, int propertyId) {
+                String s = ((ObservableField<String>) sender).get();
+                System.out.println("sender================" + s);
+                System.out.println("propertyId============" + propertyId);
+                textView1.setText(s);
             }
         });
+
     }
 
+    private void initViews() {
+        textView1 = ((TextView) this.findViewById(R.id.tv_vm1));
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.btn3).setOnClickListener(this);
+        findViewById(R.id.btn4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn1:
+                //主线程
+                viewModel.getName().setValue("11111111111111");
+                break;
+            case R.id.btn2:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //子线程
+                        viewModel.getName().postValue("2222222222222222");
+                    }
+                }).start();
+                break;
+            case R.id.btn3:
+                observableUser.name.set("333333333333333");
+                break;
+            case R.id.btn4:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        observableUser.name.set("44444444");
+                    }
+                }).start();
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/com/example/mycardview/livedataviewmodel/ObservableUser.java
@@ -0,0 +1,7 @@
+package com.example.mycardview.livedataviewmodel;
+
+import androidx.databinding.ObservableField;
+
+public class ObservableUser {
+    public final ObservableField<String> name = new ObservableField<>();
+}
